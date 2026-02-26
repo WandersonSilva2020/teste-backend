9599653145dfe8a76c2ab1c082efbf6c2897841d
Wanderson Silva <64982436+WandersonSilva2020@users.noreply.github.com>
2026-02-26 15:56:49 -0300

    feat: add front-end files

---
+++ front-end/src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3000', 
+});
+
+api.interceptors.request.use((config) => {
+  // 1. Evita o erro de SSR no Next.js
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  // 2. Injeta a sua API Key do .env
+  config.headers['x-api-key'] = process.env.NEXT_PUBLIC_API_KEY;
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
